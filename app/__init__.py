618d502f2b90b5b3302d195fb29132f29b541875
Sm0k3s <obaga4@gmail.com>
2018-03-25 19:13:32 +0300

    adds endpoint to get a single book

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -15,10 +15,24 @@ books = [
         'Synopsis':'Mario goes to save princess Peach'
     }
         ]
-
+#gets all books
 @app.route('/api/v1/books')
 def get_books():
     return jsonify({'books': books})
+
+#gets a single book by id
+@app.route('/api/v1/books/<int:book_id>', methods=['GET'])
+def get_book(book_id):
+    book = [book for book in books if book['id'] == book_id]
+    if len(book) == 0:
+        abort(404)
+    return jsonify({'book':book[0]})
+
+#converts error 404 into json
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
